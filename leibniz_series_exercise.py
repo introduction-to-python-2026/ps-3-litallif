63e7212dfa9f0365fdbe6ad80c404390af88344d
litallif <liflital452@gmail.com>
2025-11-12 13:35:55 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,8 +1,5 @@
-def approximate_pi():
-    n_terms = int(input("how many numbers?"))
-    list_of_numbers = []
+def approximate_pi(n_terms):
+    sum=0
     for i in range(n_terms):
-        list_of_numbers.append((-1)**i/(2*i+1))
-    pi = sum(list_of_numbers) * 4
-    return pi
-approximate_pi()
+        sum=sum+(-1)**i/(2*i+1)
+    terurn 4*sum
